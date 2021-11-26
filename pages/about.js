868b246a12f240b2a0976d07059788b1f799db8a
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-11-26 21:46:35 +0100

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest news and make a comprehensive vision of them</p>
+        </div>
+    )
+}
---
+++ pages/news/add.js
@@ -0,0 +1,7 @@
+export default function AddNewsPage() {
+    return (
+        <div>
+            <h1>Add News</h1>
+        </div>
+    )
+}
