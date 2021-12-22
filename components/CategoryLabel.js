ece9896432738b720b5508573be202aae78a4933
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-12-22 19:57:02 +0100

    small design issues

--- components/CategoryLabel.js
+++ components/CategoryLabel.js
@@ -4,7 +4,7 @@ export default function CategoryLabel({ children }) {
   const colorKey = {
     Politics: 'qblue',
     Business: 'qgreydark',
-    Technology: 'metal',
+    Technology: 'qorange',
     Culture: 'alyminium',
     Sport: 'qte',
     Health: 'steel'
