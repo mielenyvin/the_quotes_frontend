22acca81dd85b9ac310893a9cfb3710135983cac
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-12-22 11:19:16 +0100

    small design issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,7 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'qte': '#14213d',
-        'metal': '#565584',
-        'silver': '#evebff',
-        'alyminium': '#161616',
-        'steel': '#424242',
-        'black': '#000000',
-        'qblue': '#14213d',
-        'qellow': '#fca311',
-        'qgrey': '#e5e5e5',
-        'qgreydark': '#999999',
-        'qred': '#a4161a',
-        'qorange': '#dc2f02',
-        'qgreen': '#76c893',
-        'qviolet': '#cbb2fe',
-        'qlitegray': '#e0e1dd',
-        'qverylitegray': '#fafafa',
-        'qveryverylitegray' :'#fdfdfd'
-      },
-
-    },
+    extend: {},
   },
   variants: {
     extend: {
@@ -37,4 +12,3 @@ module.exports = {
   },
   plugins: [],
 }
-
