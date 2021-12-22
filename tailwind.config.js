ef25d84f6c7d6217bc7dabede69ddc65858b5a24
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-12-22 11:12:04 +0100

    small design issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
@@ -26,6 +26,8 @@ module.exports = {
         'qverylitegray': '#fafafa',
         'qveryverylitegray' :'#fdfdfd'
       },
+
+    },
   },
   variants: {
     extend: {
@@ -35,3 +37,4 @@ module.exports = {
   },
   plugins: [],
 }
+
