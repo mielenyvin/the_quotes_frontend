b41d43d35714348bcad715f033bb13edc1912a19
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-12-28 12:04:45 +0100

    small design issues

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,11 +1,11 @@
 export default function Showcase() {
     return (
-<div  className="p-8 bg-qgreydark text-black font-semibold text-center">
+<div  className="p-8 bg-qgrey text-black font-semibold text-center">
 
 {/* id="bgstyle" bg-repeat  */}
 
           <div className="w-full flex flex-row justify-center">
-            <h2 className="w-72 bg-qgreydark text-white">Find the truth in the middle</h2></div>
+            <h2 className="w-72 bg-qgrey text-black">Find the truth in the middle</h2></div>
         </div>
     )
 }
