43134eba31437024b8548ac46655ceb082e590cf
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-07 15:51:08 +0100

    small design changes

--- pages/about.js
+++ pages/about.js
@@ -1,40 +1,21 @@
 import Layout from '@/components/Layout'
-import Image from 'next/image'
 
 export default function AboutPage() {
     return (
         <Layout title='About the Quotes'>
-            <h1 className='text-5xl pb-5 font-bold'>About the Quotes</h1>
+            <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
 
-            <div className='bg-white shadow-md p-5 rounded-lg py-6 '>
+            <div className='bg-white  rounded-lg py-6 '>
                 <h3 className='text-2xl mb-5'>
-                    the Quotes media (Version 1.0.0)
+                    the Quotes media
                 </h3>
-                <p>
+                <p className='mb-3'>
                 This is an app to find the latest news and make a comprehensive vision of them.</p>
-                <h1 className='text-2xl'>Project team:</h1>
-               <p><span className='font-bold'>Dmitrii Kotikov</span> (Russia)</p>
-                
-                <div className='flex flex-row items-center'>
-                <Image className='ml-3' className='rounded-xl ' src='/images/dmitrii.jpg' width={100} height={100} alt='DK'/>
-                <ul className="list-disc pl-10"><li>Author of idea</li><li>Static site generators front-end and back-end development</li></ul>
-                </div>
 
-                <p><span className='font-bold'>Houda Jaouhari</span> (Morocco)</p>
-                <div className='flex flex-row items-center'>
-                <Image className='ml-3' className='rounded-xl border border-qgrey' src='/images/logo.png' width={100} height={100} alt='JH'/>
-                <ul className="list-disc pl-10"><li>Marketing researcher</li><li>Project tester</li></ul>
-                </div>
-
-                <p><span className='font-bold'>Prabhmeet Singh Bhatti</span> (India)</p>
-
-                <div className='flex flex-row items-center'>
-                <Image className='ml-3' className='rounded-xl' src='/images/prabhmeet.jpg' width={100} height={100} alt='PS'/>
-                <ul className="list-disc pl-10"><li>Development and maintenance of the Android/iOS Applications</li></ul>
-                </div>
-               
-               
-
+                <p>
+                    
+                    <span className='font-bold'>Version 1.0.0</span>
+                </p>
             </div>
 
           
