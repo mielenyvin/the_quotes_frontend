a02614848b444a45cbf010e75400693df744f992
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-07 15:56:20 +0100

    small design changes2

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function AboutPage() {
 
             <div className='bg-white  rounded-lg py-6 '>
                 <h3 className='text-2xl mb-5'>
-                    the Quotes media
+                    the Quotes
                 </h3>
                 <p className='mb-3'>
                 This is an app to find the latest news and make a comprehensive vision of them.</p>
