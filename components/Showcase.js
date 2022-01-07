1f2af3b805a309c94fcede7b3abe0c5b7ddfe72d
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-07 15:22:45 +0100

    small design changes

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,11 +1,13 @@
 export default function Showcase() {
     return (
-<div  className="p-8 bg-qgrey text-black font-semibold text-center">
+<div  id="bgstyle"  className="p-8 bg-alyminium text-black font-semibold text-center">
+
+{/* <img className="" src='/images/download.svg' /> */}
 
 {/* id="bgstyle" bg-repeat  */}
 
           <div className="w-full flex flex-row justify-center">
-            <h2 className="w-72 bg-qgrey text-black">Find the truth in the middle</h2></div>
+    <h2 className="w-72 bg-transparent  text-white"> Find the truth in the middle</h2></div>
         </div>
     )
 }
