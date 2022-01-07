c8bd2186c0806a41c10e516d4c43329da1cc23e7
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-07 14:11:31 +0100

    design changes

--- pages/about.js
+++ pages/about.js
@@ -3,19 +3,24 @@ import Layout from '@/components/Layout'
 export default function AboutPage() {
     return (
         <Layout title='About the Quotes'>
-            <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
+            <h1 className='text-5xl pb-5 font-bold'>About the Quotes</h1>
 
-            <div className='bg-white  rounded-lg py-6 '>
+            <div className='bg-white shadow-md p-5 rounded-lg py-6 '>
                 <h3 className='text-2xl mb-5'>
-                    the Quotes media
+                    the Quotes media (Version 1.0.0)
                 </h3>
-                <p className='mb-3'>
-                This is an app to find the latest news and make a comprehensive vision of them.</p>
-
                 <p>
-                    
-                    <span className='font-bold'>Version 1.0.0</span>
-                </p>
+                This is an app to find the latest news and make a comprehensive vision of them.</p>
+                <h1 className='text-2xl'>Project team:</h1>
+               <p className='font-bold'>Dmitrii Kotikov (Russia)</p>
+                <img className='ml-3' className='rounded-2xl' src='/images/dmitrii.jpg' width={100} height={100} alt='logo'/>
+                <p  className='font-bold'>Houda Jaouhari (Morocco)</p>
+                <img className='ml-3' className='rounded-2xl' src='/images/dmitrii.jpg' width={100} height={100} alt='logo'/>
+                <p  className='font-bold'>Prabhmeet Singh Bhatti (India)</p>
+                <img className='ml-3' className='rounded-2xl' src='/images/dmitrii.jpg' width={100} height={100} alt='logo'/>
+               
+               
+
             </div>
 
           
