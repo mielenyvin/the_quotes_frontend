83f540697b8436edad78a99025b910440f433259
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-07 16:21:04 +0100

    small changes

--- pages/about.js
+++ pages/about.js
@@ -6,16 +6,14 @@ export default function AboutPage() {
             <h1 className='text-5xl border-b-4 pb-5 font-bold'>About us</h1>
 
             <div className='bg-white  rounded-lg py-6 '>
-                <h3 className='text-2xl mb-5'>
-                    the Quotes
+            <h3 className='text-2xl mb-5'>
+                    the Quotes media (Version 1.0.0)
                 </h3>
                 <p className='mb-3'>
                 This is an app to find the latest news and make a comprehensive vision of them.</p>
+                <h1 className='text-2xl'>Project team:</h1>
+                <p><span className='font-bold'>Dmitrii Kotikov</span> (Russia)</p>
 
-                <p>
-                    
-                    <span className='font-bold'>Version 1.0.0</span>
-                </p>
             </div>
 
           
